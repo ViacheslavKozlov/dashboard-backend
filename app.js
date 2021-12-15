e386a21e701c1bffb8b2f02262dde0a46890cc3c
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-12-15 12:01:00 +0100

    cors http added

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://stupefied-boyd-4fa663.netlify.app");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
