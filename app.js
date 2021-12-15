657604b8a7a02cd8dbb4dafa26908b4a25342fd3
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-12-15 12:07:17 +0100

    roll back

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://stupefied-boyd-4fa663.netlify.app");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+//   next();
+// });
 
 app.use("/api/users", usersRouter);
 app.use("/api/tasks", tasksRouter);
